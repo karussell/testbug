cae606a7f5510becd831140ee63e6891f309cadc
Peter <graphhopper@gmx.de>
2019-01-22 15:30:57 +0100

    move tif read before subnetwork removal

--- src/main/java/testbug/Test.java
+++ src/main/java/testbug/Test.java
@@ -26,23 +26,34 @@ public class Test {
         CmdArgs argsFromProps = CmdArgs.read(Helper.readFile("config.properties").toArray(new String[]{}));
         argsFromProps.merge(CmdArgs.read(args));
         logger.info("read " + argsFromProps);
-        GraphHopper graphHopper = new GraphHopperOSM().init(argsFromProps);
+        GraphHopper graphHopper = new GraphHopperOSM() {
+            @Override
+            protected void cleanUp() {
+                List<Raster> list = new ArrayList<>();
+
+                try {
+                    // read it twice to be sure that we trigger the bug
+                    for (int i = 0; i < 2; i++) {
+                        for (String rasterFile : Arrays.asList("i0.tif", "i1.tif", "i2.tif", "i3.tif", "i4.tif", "i5.tif", "i6.tif", "i7.tif", "i8.tif")) {
+                            logger.info(new Date() + " " + i + " reading ... " + rasterFile + " " + Helper.getMemInfo());
+                            BufferedImage image = ImageIO.read(new BufferedInputStream(new FileInputStream(rasterFile), 5000));
+                            list.add(image.getRaster());
+                        }
+                    }
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                logger.info(new Date() + " read " + list.size() + " finished");
+                super.cleanUp();
+            }
+        }.init(argsFromProps);
         if (graphHopper.getElevationProvider() == ElevationProvider.NOOP)
             throw new RuntimeException("Enable the elevation provider as otherwise the bug is not triggered");
 
+        logger.info("start " + new Date().toString());
         graphHopper.importOrLoad();
-        List<Raster> list = new ArrayList<>();
-
-        // read it twice to be sure that we trigger the bug
-        for (int i = 0; i < 2; i++) {
-            for (String rasterFile : Arrays.asList("i0.tif", "i1.tif", "i2.tif", "i3.tif", "i4.tif", "i5.tif", "i6.tif", "i7.tif", "i8.tif")) {
-                logger.info(i + " reading ... " + rasterFile + " " + Helper.getMemInfo());
-                BufferedImage image = ImageIO.read(new BufferedInputStream(new FileInputStream(rasterFile), 5000));
-                list.add(image.getRaster());
-            }
-        }
-
-        logger.info(new Date() + " read " + list.size() + " finished");
+        logger.info("end" + new Date().toString());
         graphHopper.close();
     }
 }
